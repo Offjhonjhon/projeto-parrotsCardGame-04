5da10f39883de81d3c9b624f99e5fee9a75d6b3e
João Araujo <jjvictorac@gmail.com>
2022-02-01 01:51:21 -0300

    tela inicial do game

---
+++ script.js
@@ -0,0 +1,6 @@
+let quantidadeCartas = 0;
+ /*
+while((quantidadeCartas < 2) || (quantidadeCartas > 14) || ((quantidadeCartas % 2) !== 0)){
+quantidadeCartas = prompt("Quantas cartas vc quer?");
+}
+*/
