e037fec7cfcd314b85b4e58b94566b8676129dad
João Araujo <jjvictorac@gmail.com>
2022-02-02 14:50:50 -0300

    ajuste da animação das cartas

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 let quantidadeCartas = 0;
 
-let carta = `   <div class="carta" onclick="virarCarta()">
-                    <div class="">
-                        <figure>
-                            <img src="imgs/front.png" alt="">
-                        </figure>
+let carta = `    <div class="carta" onclick="virarCarta()">
+                    <div  class="front-face face" >
+                        <img src="imgs/front.png" alt="">
+                    </div>
+                    <div  class="back-face face">
+                        <p>Parte de tras</p>
                     </div>
                 </div>
 `;
 
+virarCarta()
 
-pegarNumeroCartas();
-colocarCartas();
+// pegarNumeroCartas();
+// colocarCartas();
 
-// pega a quantidade de cartas a ser inserida 
-function pegarNumeroCartas() {
-    quantidadeCartas = prompt("Quantas cartas vc quer?");
+// // pega a quantidade de cartas a ser inserida 
+// function pegarNumeroCartas() {
+//     quantidadeCartas = prompt("Quantas cartas vc quer?");
 
-    if ((quantidadeCartas < 2) || (quantidadeCartas > 14) || ((quantidadeCartas % 2) !== 0)) {
-        return pegarNumeroCartas();
-    }
-}
-// coloca as cartas na tela
-function colocarCartas() {
-    let main = document.querySelector(".principal");
-    for (let i = 0; i < quantidadeCartas; i++) {
-        main.innerHTML += carta;
-        console.log();
-
-    }
-}
-
-// function virarCarta(){
-//     const front = document.querySelector(".carta .front");
-//     const back = document.querySelector(".carta .back");
-//     front.classList.add("front-face");
-//     back.classList.add("back-face");
+//     if ((quantidadeCartas < 2) || (quantidadeCartas > 14) || ((quantidadeCartas % 2) !== 0)) {
+//         return pegarNumeroCartas();
+//     }
 // }
+// // coloca as cartas na tela
+// function colocarCartas() {
+//     let main = document.querySelector(".principal");
+//     for (let i = 0; i < quantidadeCartas; i++) {
+//         main.innerHTML += carta;
+//     }
+// }
+
+
+
+function virarCarta() {
+    const back = document.querySelectorAll(".back-face");
+    const front = document.querySelectorAll(".front-face");
+    
+    front.style
+}
 
 
