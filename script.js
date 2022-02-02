bdd23ad24bfaad15336824661f10e3e21852b653
João Araujo <jjvictorac@gmail.com>
2022-02-02 00:55:06 -0300

    cartas adicionadas a tela conforme o usuario definir

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 let quantidadeCartas = 0;
- /*
-while((quantidadeCartas < 2) || (quantidadeCartas > 14) || ((quantidadeCartas % 2) !== 0)){
-quantidadeCartas = prompt("Quantas cartas vc quer?");
+
+let carta = `   <div class="carta" onclick="virarCarta()">
+                    <div class="">
+                        <figure>
+                            <img src="imgs/front.png" alt="">
+                        </figure>
+                    </div>
+                </div>
+`;
+
+
+pegarNumeroCartas();
+colocarCartas();
+
+// pega a quantidade de cartas a ser inserida 
+function pegarNumeroCartas() {
+    quantidadeCartas = prompt("Quantas cartas vc quer?");
+
+    if ((quantidadeCartas < 2) || (quantidadeCartas > 14) || ((quantidadeCartas % 2) !== 0)) {
+        return pegarNumeroCartas();
+    }
 }
-*/
+// coloca as cartas na tela
+function colocarCartas() {
+    let main = document.querySelector(".principal");
+    for (let i = 0; i < quantidadeCartas; i++) {
+        main.innerHTML += carta;
+        console.log();
+
+    }
+}
+
+// function virarCarta(){
+//     const front = document.querySelector(".carta .front");
+//     const back = document.querySelector(".carta .back");
+//     front.classList.add("front-face");
+//     back.classList.add("back-face");
+// }
+
+
