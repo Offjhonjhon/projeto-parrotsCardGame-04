0f98c2d66defa1b7461335ec2801caa1440a37aa
João Araujo <jjvictorac@gmail.com>
2022-02-03 08:49:23 -0300

    Gifs adicionados em todas as cartas

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
 let quantidadeCartas = 0;
+let gifs = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot','revertitparrot','tripletsparrot','unicornparrot'];
 
-let carta = `    <div class="carta" onclick="virarCarta()">
-                    <div  class="front-face face" >
-                        <img src="imgs/front.png" alt="">
+let cartaHtml = `   <div class="carta" ">
+                        <div  class="front-face face" >
+                            <img src="imgs/front.png" alt="">
+                        </div>
+                            <div  class="back-face face">
+                        </div>
                     </div>
-                    <div  class="back-face face">
-                        <p>Parte de tras</p>
-                    </div>
-                </div>
 `;
 
-virarCarta()
 
-// pegarNumeroCartas();
-// colocarCartas();
+pegarNumeroCartas();
+colocarCartas();
 
-// // pega a quantidade de cartas a ser inserida 
-// function pegarNumeroCartas() {
-//     quantidadeCartas = prompt("Quantas cartas vc quer?");
+// pega a quantidade de cartas a ser inserida 
+function pegarNumeroCartas() {
+    quantidadeCartas = prompt("Quantas cartas vc quer?");
 
-//     if ((quantidadeCartas < 2) || (quantidadeCartas > 14) || ((quantidadeCartas % 2) !== 0)) {
-//         return pegarNumeroCartas();
-//     }
-// }
-// // coloca as cartas na tela
-// function colocarCartas() {
-//     let main = document.querySelector(".principal");
-//     for (let i = 0; i < quantidadeCartas; i++) {
-//         main.innerHTML += carta;
-//     }
-// }
-
-
-
-function virarCarta() {
-    const back = document.querySelectorAll(".back-face");
-    const front = document.querySelectorAll(".front-face");
-    
-    front.style
+    if ((quantidadeCartas < 2) || (quantidadeCartas > 14) || ((quantidadeCartas % 2) !== 0)) {
+        return pegarNumeroCartas();
+    }
 }
+
+// coloca as cartas na tela
+function colocarCartas() {
+    let main = document.querySelector("main");
+    for (let i = 0; i < quantidadeCartas; i++) {
+        main.innerHTML += cartaHtml;
+    }
+    addGifs();
+}
+
+// adciona os gifs
+function addGifs(){
+    let objectCard = document.querySelectorAll(".back-face");
+    for(let i = 0; i < (quantidadeCartas/2); i++){
+        objectCard[i].innerHTML = `<img src="gifs/${gifs[i]}.gif"></img>`;
+        objectCard[i+(quantidadeCartas/2)].innerHTML = `<img src="gifs/${gifs[i]}.gif"></img>`;
+    }
+}
+
+// embaralha as cartas 
+function shuffle(){
+
+}
+
 
 
