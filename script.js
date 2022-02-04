776e92cc569342f7b53525e6e009e9b1351eedc7
João Araujo <jjvictorac@gmail.com>
2022-02-04 12:03:25 -0300

    as cartas retornam a posição inicial caso sejam diferentes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 let quantidadeCartas = 0;
 let gifs = ['bobrossparrot','bobrossparrot','explodyparrot','explodyparrot','fiestaparrot','fiestaparrot','metalparrot','metalparrot','revertitparrot','revertitparrot','tripletsparrot','tripletsparrot','unicornparrot','unicornparrot'];
 
-let cartaHtml = `   <div class="carta" onclick="turnCard(this)">
+let cartaHtml = `   <div class="carta" onclick="turnCard(this), select(this)">
                         <div class="front face" >
                             <img src="imgs/front.png" alt="">
                         </div>
 
-                        <div class="back back-face-initial face">
+                        <div class="back face">
                         </div>
                     </div>
 `;
+
+let card1 = null;
+let card2 = null;
+let parentCard1 = null;
+let parentCard2 = null;
 
 
 pegarNumeroCartas();
@@ -32,8 +37,8 @@ function colocarCartas() {
         main.innerHTML += cartaHtml;
     }
     addGifs();    
-    
 }
+
 
 // adciona os gifs
 function addGifs(){
@@ -56,6 +61,7 @@ function addGifs(){
     }
 }
 
+
 // reorganiza a array aleatoriamente
 function shuffleArray(initialArray){
     let newPosition, temporaryPosition;
@@ -71,13 +77,63 @@ function shuffleArray(initialArray){
 
 // vira a carta
 function turnCard(card){
+    // seleciona o front da carta
     let front = card.querySelector(".front");
-    front.classList.add("front-face");
-
+    front.style.transform = "rotateY(-180deg)";
+    // seleciona o back da carta
     let back = card.querySelector(".back");
-    back.classList.add("back-face");
-    back.classList.remove("back-face-initial");
+    back.style.transform = "rotateY(0)";
 } 
+
+// desvira a carta
+function unturnCard(parentCard1,parentCard2){
+
+    // desvira a primeira carta
+    let front1 = parentCard1.querySelector(".front");
+    let back1 = parentCard1.querySelector(".back")
+    front1.style.transform = "rotateY(0)";
+    back1.style.transform = "rotateY(180deg)";
+
+    // desvira a segunda carta
+    let front2 = parentCard2.querySelector(".front");
+    let back2 = parentCard2.querySelector(".back");
+    front2.style.transform = "rotateY(0)";
+    back2.style.transform = "rotateY(180deg)";
+} 
+
+
+// seleciona as cartas
+function select(card){
+
+    if(card1 === null){
+        card1 = card.querySelector(".back");
+        parentCard1 = card1.parentNode;
+        card1 = card1.innerHTML;
+        
+    }
+    else{
+        card2 = card.querySelector(".back");
+        parentCard2 = card2.parentNode;
+        card2 = card2.innerHTML;
+    }       
+
+    if(card1 !== null && card2 !== null && card1 !== card2){
+        setTimeout(unturnCard, 500, parentCard1, parentCard2);
+        card1 = null;
+        card2 = null;
+    }
+    else if(card1 !== null && card2 !== null && card1 === card2){
+        card1 = null;
+        card2 = null;
+    }
+    console.log(card1);
+    console.log(card2);
+}
+
+
+
+
+
 
 
 
